c17e4d8a1958cadaff0b400c1c72eeb64b9856fe
uga-rosa <uga6603@gmail.com>
2023-06-02 08:40:37 +0900

    Move actions to kind

--- denops/@ddu-kinds/search_history.ts
+++ denops/@ddu-kinds/search_history.ts
@@ -1,8 +1,55 @@
-import { BaseKind } from "https://deno.land/x/ddu_vim@v2.8.4/types.ts";
+import {
+  ActionArguments,
+  ActionFlags,
+  Actions,
+  BaseKind,
+} from "https://deno.land/x/ddu_vim@v2.9.2/types.ts";
+import { batch, fn } from "https://deno.land/x/ddu_vim@v2.9.2/deps.ts";
+import { register } from "https://deno.land/x/denops_std@v5.0.0/variable/register.ts";
 
 type Params = Record<never, never>;
 
+export type ActionData = {
+  command: string;
+  index: number;
+};
+
 export class Kind extends BaseKind<Params> {
+  override actions: Actions<Params> = {
+    execute: async ({ denops, items }: ActionArguments<Params>) => {
+      const action = items[0]?.action as ActionData;
+      await batch(denops, async (denops) => {
+        await fn.histadd(denops, "search", action.command);
+        await register.set(denops, "/", action.command);
+        await fn.feedkeys(denops, "n", "n");
+      });
+      return ActionFlags.None;
+    },
+    edit: async ({ denops, items }: ActionArguments<Params>) => {
+      const action = items[0]?.action as ActionData;
+      await fn.feedkeys(denops, `/${action.command}`, "n");
+      return ActionFlags.None;
+    },
+    delete: async ({ denops, items }: ActionArguments<Params>) => {
+      await batch(denops, async (denops) => {
+        for (const item of items) {
+          const action = item?.action as ActionData;
+          if (item.action) {
+            await fn.histdel(denops, "search", action.index);
+          }
+        }
+      });
+      // Note: rviminfo! is broken in Vim8 before 8.2.2494
+      if (
+        await fn.has(denops, "nvim") ||
+        await fn.has(denops, "patch-8.2.2494")
+      ) {
+        await denops.cmd("wviminfo! | rviminfo!");
+      }
+      return ActionFlags.RefreshItems;
+    },
+  };
+
   params(): Params {
     return {};
   }
--- denops/@ddu-sources/search_history.ts
+++ denops/@ddu-sources/search_history.ts
@@ -1,21 +1,6 @@
-import {
-  ActionArguments,
-  ActionFlags,
-  BaseSource,
-  Item,
-} from "https://deno.land/x/ddu_vim@v2.8.4/types.ts";
-import {
-  batch,
-  Denops,
-  fn,
-  gather,
-} from "https://deno.land/x/ddu_vim@v2.8.4/deps.ts";
-import { register } from "https://deno.land/x/denops_std@v4.3.3/variable/register.ts";
-
-export type ActionData = {
-  command: string;
-  index: number;
-};
+import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v2.9.2/types.ts";
+import { Denops, fn, gather } from "https://deno.land/x/ddu_vim@v2.9.2/deps.ts";
+import { ActionData } from "../@ddu-kinds/search_history.ts";
 
 type Params = Record<never, never>;
 
@@ -52,41 +37,6 @@ export class Source extends BaseSource<Params> {
       },
     });
   }
-
-  actions = {
-    execute: async ({ denops, items }: ActionArguments<Params>) => {
-      const action = items[0]?.action as ActionData;
-      await batch(denops, async (denops) => {
-        await fn.histadd(denops, "search", action.command);
-        await register.set(denops, "/", action.command);
-        await fn.feedkeys(denops, "n", "n");
-      });
-      return Promise.resolve(ActionFlags.None);
-    },
-    edit: async ({ denops, items }: ActionArguments<Params>) => {
-      const action = items[0]?.action as ActionData;
-      await fn.feedkeys(denops, `/${action.command}`, "n");
-      return Promise.resolve(ActionFlags.None);
-    },
-    delete: async ({ denops, items }: ActionArguments<Params>) => {
-      await batch(denops, async (denops) => {
-        for (const item of items) {
-          const action = item?.action as ActionData;
-          if (item.action) {
-            await fn.histdel(denops, "search", action.index);
-          }
-        }
-      });
-      // Note: rviminfo! is broken in Vim8 before 8.2.2494
-      if (
-        await fn.has(denops, "nvim") ||
-        await fn.has(denops, "patch-8.2.2494")
-      ) {
-        await denops.cmd("wviminfo! | rviminfo!");
-      }
-      return Promise.resolve(ActionFlags.RefreshItems);
-    },
-  };
 
   params(): Params {
     return {};
